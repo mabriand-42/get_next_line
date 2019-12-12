56a8fc4e6d6aef5764177aa4eb9c076ba36604b4
Marie BRIAND <mabriand@e2r6p5.42.fr>
2019-12-12 14:18:49 +0100

    First Commit, GNL ok

---
+++ get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabriand <mabriand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/22 17:27:21 by mabriand          #+#    #+#             */
+/*   Updated: 2019/12/12 11:08:17 by mabriand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <fcntl.h>
+
+/*
+**STRUCURE
+*/
+
+/*
+**PROTOTYPE
+*/
+int			get_next_line(int fd, char **line);
+size_t		ft_strlen(char *s);
+char		*ft_strjoin(char *s1, char *s2);
+ssize_t		ft_search_line(char *s);
+char		*ft_strdup(char *s1);
+char		*ft_strndup(char *s1, size_t size);
+void		ft_reinit_buffer(ssize_t start, char *buffer);
+char		*ft_create_line(char *str, char **line);
+char		*ft_strcpy(char *dest, char *src);
+
+#endif
